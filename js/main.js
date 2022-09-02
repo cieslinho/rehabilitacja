930670bd1b3028e99df20c49f496ad831abe5836
Szymon Cieśla <cszymon01@gmail.com>
2022-09-02 17:40:19 +0200

    fixed js, changed nav desktop and mobile

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ const navItems = document.querySelectorAll('.nav__itemMobile')
 
 const handleNav = () => {
 	navMenu.classList.toggle('nav__listMobile-active')
+	navItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navMenu.classList.remove('nav__listMobile-active')
+		})
+	})
 }
 
 navBtn.addEventListener('click', handleNav)
