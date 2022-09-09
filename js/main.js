dccc8b20baa33ecafd79e5ee91e6bbee79ac6da1
Szymon Cieśla <cszymon01@gmail.com>
2022-09-09 15:31:17 +0200

    finished website, details to edit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ const navMenu = document.querySelector('.nav__listMobile')
 const navItems = document.querySelectorAll('.nav__itemMobile')
 const accordionBtn = document.querySelector('.gallery__accordion-btn')
 const gallery = document.querySelector('.gallery__boxes')
+const footerYear = document.querySelector('.footer__year')
+const loadMoreBtn = document.querySelector('.offer__btn')
+
+let currentItem = 3
+
+loadMoreBtn.onclick = () => {
+	let offerOption = [...document.querySelectorAll('.offer__option')]
+	for (var i = currentItem; i < currentItem + 3; i++) {
+		offerOption[i].style.display = 'flex'
+	}
+	currentItem += 3
+	if (currentItem >= offerOption.length) {
+		loadMoreBtn.style.display = 'none'
+	}
+}
 
 const handleNav = () => {
 	navMenu.classList.toggle('nav__listMobile-active')
@@ -15,6 +30,11 @@ const handleNav = () => {
 const handleAccordion = () => {
 	gallery.classList.toggle('active')
 }
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
 
-accordionBtn.addEventListener('click', handleAccordion)
 navBtn.addEventListener('click', handleNav)
+accordionBtn.addEventListener('click', handleAccordion)
+handleCurrentYear()
